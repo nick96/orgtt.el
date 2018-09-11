08224cdf46deec4f6fde2599e970d90e26da7f9a
Nick Spain <nicholas.spain96@gmail.com>
2018-09-11 12:05:42 +1000

    Add helper functions

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,3 +1,22 @@
 ;;; test-helper.el --- Helpers for orgtt.el-test.el
 
+;;; Commentary:
+
+;;; Code:
+
+(defun test-helper-get-table (n)
+  "Get contents of file 'expected-tables/tabel$N.org'."
+  (let ((table-name (format "expected-tables/table%d.org" n)))
+    (with-temp-buffer
+      (insert-file-contents table-name)
+      (buffer-substring-no-properties (point-min) (point-max)))))
+
+(defun test-helper-get-solved-table (n)
+  "Get contents of file 'expected-tables/solved-tabel$N.org'."
+  (let ((table-name (format "expected-tables/solved-table%d.org" n)))
+    (with-temp-buffer
+      (insert-file-contents table-name)
+      (buffer-substring-no-properties (point-min) (point-max)))))
+
+(provide 'test-helper)
 ;;; test-helper.el ends here
