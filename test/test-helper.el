8d6fb6522a718d74fe64219d5817ef02c33997f7
Nick Spain <nicholas.spain96@gmail.com>
2018-09-21 22:07:30 +1000

    Make path to table files absolute so they can be run from anywhere

--- test/test-helper.el
+++ test/test-helper.el
@@ -4,16 +4,27 @@
 
 ;;; Code:
 
+(require 'f)
+;; I know this is gross but I need a way of being able to easily refer
+;; to the absolute file name so that tests can be run interactively in
+;; emacs and from cask.
+(require 'projectile)
+
+
 (defun test-helper-get-table (n)
   "Get contents of file 'expected-tables/tabel$N.org'."
-  (let ((table-name (format "test/expected-tables/table%d.org" n)))
+  (let ((table-name (format (f-join (projectile-project-root)
+				    "test/expected-tables/table%d.org")
+			    n)))
     (s-chomp (with-temp-buffer
 	       (insert-file-contents table-name)
 	       (test-helper-buffer-no-properties)))))
 
 (defun test-helper-get-solved-table (n)
   "Get contents of file 'expected-tables/solved-tabel$N.org'."
-  (let ((table-name (format "test/expected-tables/solved-table%d.org" n)))
+  (let ((table-name (format (f-join (projectile-project-root)
+				    "test/expected-tables/solved-table%d.org")
+			    n)))
     (s-chomp (with-temp-buffer
 	       (insert-file-contents table-name)
 	       (test-helper-buffer-no-properties)))))
